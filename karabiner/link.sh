4c8bf213111bca8336b8de24a39a3e3b28318f58
aaashuai <nsity223@gmail.com>
2021-12-08 11:17:45 +0800

    Add: karabiner config

---
+++ karabiner/link.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+karabinerConfigPath=/Users/lin/.config/karabiner/assets/complex_modifications
+if [[ ! -d  "$karabinerConfigPath" ]]
+then
+    echo 'path not exist, exit'
+    exit 1
+fi
+
+for i in $(ls |grep .json)
+do 
+    echo ln -s $(pwd)/${i} ${karabinerConfigPath}/${i}
+
+    ln -s $(pwd)/${i} ${karabinerConfigPath}/${i}
+done
