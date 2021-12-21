2ff9d8b939abb20f7fe875aaebe94afa24304f6b
aaashuai <nsity223@gmail.com>
2021-12-21 19:19:29 +0800

    Update: yabai

--- yabai/link.sh
+++ yabai/link.sh
@@ -3,6 +3,9 @@
 # link config files to $HOME
 
 pwd=`pwd`
-ln -sf ${pwd}/spacebarrc ~/.spacebarrc
+if [ ! -d "~/.config/spacebar" ]; then
+    mkdir ~/.config/spacebar
+fi
+ln -sf ${pwd}/spacebarrc ~/.config/spacebar/spacebarrc
 ln -sf ${pwd}/yabairc ~/.yabairc
 ln -sf ${pwd}/skhdrc ~/.skhdrc
