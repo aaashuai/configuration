c217180e553ef880f4bb38b067592681d54ecdb6
aaashuai <nsity223@gmail.com>
2022-08-09 23:57:05 +0800

    Update: nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,10 +54,13 @@ vim.o.rnu = true
 -- Save undo history
 vim.o.undofile = true
 -- Create bakcup file
-vim.o.backup = false
+-- vim.o.backup = false
+vim.o.backupskip = "/tmp/*,~/tmp/*"
 -- no swapfile
 vim.o.swapfile = false
 -- encoding
+vim.scriptencoding = "utf-8"
+vim.o.encoding = "utf-8"
 vim.o.fileencoding = "utf-8"
 -- highlight all matches on previous search pattern
 vim.o.hlsearch = true
@@ -65,6 +68,7 @@ vim.o.hlsearch = true
 vim.o.showtabline = 1
 -- make indenting smarter again
 vim.o.smartindent = true
+vim.o.autoindent = true
 -- smart case
 vim.o.smartcase = true
 -- force all horizontal splits to go below current window
@@ -75,16 +79,36 @@ vim.o.splitright = true
 vim.o.updatetime = 300
 -- convert tab to space
 vim.o.expandtab = true
--- highlight the current line
-vim.o.cursorline = true
 -- display lines as one long line 
 vim.o.wrap = false
 -- always show the sign column
 vim.o.signcolumn = "yes"
--- keep 8 lines in the end when scroll down and scroll left
-vim.o.scrolloff = 8
+-- keep x lines in the end when scroll down and scroll left
+vim.o.scrolloff = 999
 vim.o.sidescrolloff = 8
-vim.o.showcmd = false
+-- show typing command on status bar
+vim.o.showcmd = true
+
+vim.o.cmdheight = 1
+vim.o.shell = "zsh"
+vim.o.title = true
+vim.o.inccommand = 'split'
+
+vim.o.smarttab = true  -- use with shiftwidth, front of a line, use shiftwidth. in other places, use tabstop or softtabstop
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.breakindent = true -- vague
+
+vim.o.wildignore = "*/node_modules/*,*/__pycache__/*,*/venv/*"
+
+vim.o.clipboard = 'unnamedplus'
+
+-- [[ highlight ]]
+-- highlight the current line
+vim.o.cursorline = true
+vim.o.pumblend = 5
+vim.o.background = 'dark'
+
 
 -- [[ Basic Keymaps ]]
 -- Set <space> as the leader key
@@ -110,9 +134,9 @@ vim.g.maplocalleader = ' '
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<C-a>", "gg<S-v>G")
 
 -- Insert --
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
-
 
