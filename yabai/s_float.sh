7515b4278408c3d9de07dd05a2af834e716c518f
linshuai <nsity223@gmail.com>
2023-05-26 14:29:17 +0800

    Update yabai

--- yabai/s_float.sh
+++ yabai/s_float.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-# echo $YABAI_WINDOW_ID >> /Users/lin/yabai_id
+debug=false
+if [ $debug ]; then 
+   echo 1 $YABAI_WINDOW_ID
+fi
+
 : ${SLEEP_SECOND:=1}
 title=$(yabai -m query --windows --window ${YABAI_WINDOW_ID} | jq .title | cut -d [ -f 2 | cut -d ] -f 1)
-# echo $(yabai -m query --windows --window ${YABAI_WINDOW_ID}) >> /Users/lin/yabai_window
+if [ $debug ];then
+  echo 2 ${title}
+fi
 
 for i in {1..100}
 do
@@ -13,21 +19,29 @@ do
 	title=$(yabai -m query --windows --window ${YABAI_WINDOW_ID} | jq .title | cut -d [ -f 2 | cut -d ] -f 1)
   fi
 done
+if [ $debug ]; then
+  echo 3 ${title} 
+fi
 
-title="${title/\~/$HOME}"
-# echo $title >> /Users/lin/yabai_title
-# echo "done" >> /Users/lin/yabai_fin_for
+title="${title/~/$HOME}"
+if [ $debug ]; then
+  echo 4 ${title} 
+fi
 
 if [ "$title" == "." ]; then
   absolute=$(pwd)
 elif [ "$title" == ".." ]; then
   absolute=$(dirname "$(pwd)")
 else
-  absolute=$(cd "$(dirname "$title")"; pwd)/$(basename "$title")
+	# absolute=$(cd "$(dirname "${title}")"; pwd)/$(basename "$title")
+	absolute=$(cd "$(dirname "${title/'~'/$HOME}")"; pwd)/$(basename "$title")
 fi
-# echo $absolute >> /Users/lin/yabai_tmp
+
+
 if [ -e $absolute ];then
-#   echo "${YABAI_WINDOW_ID} toggled" >> /Users/lin/yabai_toggle
+if [ $debug ]; then
+      echo 5 do $absolute 
+fi
   yabai -m window ${YABAI_WINDOW_ID} --toggle float
 fi
 
