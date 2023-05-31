b0b167304847c57658830b0a1160c6b4b0642699
aaashuai <nsity223@gmail.com>
2023-05-31 23:55:24 +0800

    update

--- nvim/lua/configs/packer.lua
+++ nvim/lua/configs/packer.lua
@@ -8,18 +8,20 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+          'nvim-telescope/telescope.nvim', tag = '0.1.1',
+          -- or                            , branch = '0.1.x',
+          requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   use({ 'rose-pine/neovim', as = 'rose-pine' , config=function()
-	  vim.cmd('colorscheme rose-pine')
+          vim.cmd('colorscheme rose-pine')
   end})
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
   use('nvim-treesitter/playground')
-
+  use('ThePrimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
 
 end)
