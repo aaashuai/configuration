3d8cec4f0f3b534991ca47d42a17a8ca53626059
linshuai <nsity223@gmail.com>
2023-05-31 20:19:46 +0800

    update nvim

--- nvim/lua/configs/packer.lua
+++ nvim/lua/configs/packer.lua
@@ -17,4 +17,9 @@ return require('packer').startup(function(use)
 	  vim.cmd('colorscheme rose-pine')
   end})
 
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+  use('nvim-treesitter/playground')
+
+
 end)
